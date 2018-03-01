fa2fd2ff25ea895da4dcf02ab4775bcf784bbb09
Matthias De Fre <matthias.defre.y7363@student.hogent.be>
2018-03-01 16:52:02 +0100

    Changed classes after renaming usecaseexercisecontroller to a shorter, better name

--- BreakoutGame/src/gui/SceneFactory.java
+++ BreakoutGame/src/gui/SceneFactory.java
@@ -8,7 +8,7 @@ package gui;
 import domain.ListStudentController;
 import domain.SceneName;
 import static domain.SceneName.*;
-import domain.UseCaseExerciseAdminController;
+import domain.ExerciseController;
 import gui.ComplexApplication2.ComplexApplication_cssController;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,10 +22,10 @@ import javafx.scene.Scene;
 public class SceneFactory {
     private final Map<SceneName, Supplier<Scene>> factory = new HashMap();
 
-    public SceneFactory(UseCaseExerciseAdminController dc) {
+    public SceneFactory(ExerciseController dc) {
         add(STARTSCREEN, () -> new Scene(new StartScreenController(dc)));
         add(EXERCISESCREEN, () -> new Scene(new ExerciseDetailScreenController(dc)));
-        add(EXERCISEMAINSCREEN, () -> new Scene(new ExerciseController(dc)));
+        add(EXERCISEMAINSCREEN, () -> new Scene(new gui.ExerciseController(dc)));
         add(EXERCISEMAINSCREEN2, () -> new Scene(new ComplexApplication_cssController(dc)));
     }
     
