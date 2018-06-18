8373f3734f5e901fec7256b059452fd842bd9566
Alexander <alexander.willems.y9744@student.hogent.be>
2018-06-18 12:29:19 +0200

    mainscreen

--- BreakoutGame/src/gui/MainScreenController.java
+++ BreakoutGame/src/gui/MainScreenController.java
@@ -10,6 +10,7 @@ import domain.ExerciseDomainController;
 import domain.SceneName;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Calendar;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.ResourceBundle;
@@ -25,6 +26,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -42,17 +45,21 @@ public class MainScreenController extends AnchorPane implements Observer {
     @FXML
     private JFXButton btnExercise;
     @FXML
-    private HBox hBox;
-    @FXML
     private Label lblTitle;
     @FXML
-    private Label lblTeam;
-    @FXML
-    private Label lblVersion;
-    @FXML
-    private JFXButton btnStudents;
-    @FXML
     private JFXButton btnSession;
+    @FXML
+    private StackPane stpVoorAlert;
+    @FXML
+    private HBox hboxContent;
+    @FXML
+    private VBox vboxNavigatie;
+    @FXML
+    private VBox vboxGreeting;
+    @FXML
+    private Label lblDatum;
+    @FXML
+    private Label lblInstruction;
 
     public MainScreenController(ExerciseDomainController dc) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
@@ -65,16 +72,20 @@ public class MainScreenController extends AnchorPane implements Observer {
         }
 
         this.dc = dc;
-        //     hBox.setAlignment(Pos.CENTER);
-        //      lblTitle.setAlignment(Pos.TOP_CENTER);
-        //      lblTeam.setAlignment(Pos.BOTTOM_RIGHT);
-        //      lblVersion.setAlignment(Pos.BOTTOM_LEFT);
         this.requestFocus();
-        String image = MainScreenController.class.getResource("boeken.jpg").toExternalForm();
+        String image = MainScreenController.class.getResource("background.jpg").toExternalForm();
         this.setStyle("-fx-background-image: url('" + image + "'); "
                 + "-fx-background-position: center center; "
                 + "-fx-background-repeat: stretch;");
+
+        lblDatum.setText("Het is nu " + getUur() + " uur.");
     }
+    
+    	private String getUur() {
+		Calendar nu = Calendar.getInstance();
+		String uur = nu.get(Calendar.HOUR_OF_DAY) + ":" + nu.get(Calendar.MINUTE);
+		return uur;
+	}
 
     @FXML
     private void btnBoxStartScreen(ActionEvent event) {
@@ -93,9 +104,8 @@ public class MainScreenController extends AnchorPane implements Observer {
         SceneController4.switchScene(SceneName.EXERCISEMAINSCREEN2);
     }
 
-    @FXML
     private void btnStudentsExcell(ActionEvent event) {
-            SceneController4.createScene(SceneName.STUDENTSSCREEN);
+        SceneController4.createScene(SceneName.STUDENTSSCREEN);
         SceneController4.switchScene(SceneName.STUDENTSSCREEN);
     }
 
