fdadf6251570fb02856e154faf47406bee33fa1d
Alexander <alexander.willems.y9744@student.hogent.be>
2018-06-18 18:21:11 +0200

    minor changes

--- BreakoutGame/src/gui/ExercisesPaneLeftController.java
+++ BreakoutGame/src/gui/ExercisesPaneLeftController.java
@@ -71,16 +71,17 @@ public class ExercisesPaneLeftController extends AnchorPane {
     @FXML
     private JFXColorPicker colorPicker;
 
-    private JFXDialog dialog; 
+    private JFXDialog dialog;
     @FXML
     private Label lblOnderwerp;
     @FXML
     private JFXNodesList nodeList;
-    
-     private static final String FX_TEXT_FILL_WHITE = "-fx-text-fill:WHITE";
+
+    private static final String FX_TEXT_FILL_WHITE = "-fx-text-fill:WHITE";
     private static final String ANIMATED_OPTION_BUTTON = "animated-option-button";
     private static final String ANIMATED_OPTION_SUB_BUTTON = "animated-option-sub-button";
     private static final String ANIMATED_OPTION_SUB_BUTTON2 = "animated-option-sub-button2";
+
     public ExercisesPaneLeftController(ExerciseDomainController dc, JFXDialog dialog) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("ExercisesPaneLeft.fxml"));
         loader.setRoot(this);
@@ -101,37 +102,34 @@ public class ExercisesPaneLeftController extends AnchorPane {
                 setSelectedItemToDc();
             }
         });
-        
+
         btnDeleteExercise.disableProperty().bind(Bindings.size(tblExercises.getItems()).isEqualTo(0));
         btnCopyExercise.disableProperty().bind(Bindings.size(tblExercises.getItems()).isEqualTo(0));
-     
-       
-         JFXButton ssbutton1 = new JFXButton();
-     
+
+        JFXButton ssbutton1 = new JFXButton();
 
         JFXButton ssbutton2 = new JFXButton("A");
-       Label lblA = new Label("A");
+        Label lblA = new Label("A");
         lblA.setStyle("-fx-font-weight: bold;-fx-text-fill:WHITE");
         //lblA.setPadding(new Insets(0, 0, 0, 5));
         lblA.setAlignment(Pos.CENTER);
         ssbutton2.setGraphic(lblA);
-        
+
         ssbutton2.setButtonType(JFXButton.ButtonType.RAISED);
         ssbutton2.getStyleClass().addAll(ANIMATED_OPTION_BUTTON, ANIMATED_OPTION_SUB_BUTTON2);
-       
+
         nodeList.setSpacing(10);
-         btnCreateExercise.getStyleClass().addAll(ANIMATED_OPTION_BUTTON, ANIMATED_OPTION_SUB_BUTTON2);
-          btnCopyExercise.getStyleClass().addAll(ANIMATED_OPTION_BUTTON, ANIMATED_OPTION_SUB_BUTTON2);
-           btnDeleteExercise.getStyleClass().addAll(ANIMATED_OPTION_BUTTON, ANIMATED_OPTION_SUB_BUTTON2);
-          
+        btnCreateExercise.getStyleClass().addAll(ANIMATED_OPTION_BUTTON, ANIMATED_OPTION_SUB_BUTTON2);
+        btnCopyExercise.getStyleClass().addAll(ANIMATED_OPTION_BUTTON, ANIMATED_OPTION_SUB_BUTTON2);
+        btnDeleteExercise.getStyleClass().addAll(ANIMATED_OPTION_BUTTON, ANIMATED_OPTION_SUB_BUTTON2);
+
         nodeList.addAnimatedNode(ssbutton2);
         nodeList.addAnimatedNode(btnCreateExercise);
         nodeList.addAnimatedNode(btnCopyExercise);
-       nodeList.addAnimatedNode(btnDeleteExercise);
-         
+        nodeList.addAnimatedNode(btnDeleteExercise);
 
-     nodeList.setRotate(180);
-    
+        nodeList.setRotate(180);
+
     }
 
     @FXML
@@ -145,7 +143,7 @@ public class ExercisesPaneLeftController extends AnchorPane {
     private void btnCopyExerciseOnClick(MouseEvent event) {
         dc.setManagerMode(Exercise.class, PersistMode.NEW);
         Exercise exercise = new Exercise(tblExercises.getSelectionModel().getSelectedItem());
-      //  box.copy();
+        //  box.copy();
         exercise.setName(exercise.getName() + "_" + "COPY");
         dc.setSelectedItem(exercise);
         nodeList.animateList();
@@ -153,15 +151,15 @@ public class ExercisesPaneLeftController extends AnchorPane {
 
     @FXML
     private void btnDeleteExerciseOnClick(MouseEvent event) {
-        
-        try{
-             dc.deleteExercise();
-           //   dc.setSelectedItem(new Exercise());
-              nodeList.animateList();
-        } catch(Exception ex) {
+
+        try {
+            dc.deleteExercise();
+            //   dc.setSelectedItem(new Exercise());
+            nodeList.animateList();
+        } catch (Exception ex) {
             setErrorDialog(ex);
         }
-      
+
     }
 
     private void setSelectedItemToDc() {
@@ -171,18 +169,18 @@ public class ExercisesPaneLeftController extends AnchorPane {
 
     @FXML
     private void colorPickerOnAction(ActionEvent event) {
-        Color selectedColor =  colorPicker.getValue();
+        Color selectedColor = colorPicker.getValue();
         tblExercises.setBackground(new Background(new BackgroundFill(Paint.valueOf(selectedColor.toString()), CornerRadii.EMPTY, Insets.EMPTY)));
     }
 
-         private void setErrorDialog(Exception ex) {
-         JFXDialogLayout layout = new JFXDialogLayout();
-            layout.setBody(new Label(ex.getMessage()));
-            JFXButton okButton = new JFXButton("OK");
-            okButton.setOnMouseClicked(e -> dialog.close());
-              okButton.setStyle("-fx-background-color: #112959;");
-            layout.setActions(okButton);
-            dialog.setContent(layout);
-            dialog.show();
+    private void setErrorDialog(Exception ex) {
+        JFXDialogLayout layout = new JFXDialogLayout();
+        layout.setBody(new Label(ex.getMessage()));
+        JFXButton okButton = new JFXButton("OK");
+        okButton.setOnMouseClicked(e -> dialog.close());
+        okButton.setStyle("-fx-background-color: #112959;");
+        layout.setActions(okButton);
+        dialog.setContent(layout);
+        dialog.show();
     }
 }
